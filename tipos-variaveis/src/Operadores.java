a58a195e47f563b853907e0833cfdd58be64f718
Joao <andrade.joaomoraes@gmail.com>
2024-05-27 20:30:21 -0300

    segunda

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,21 @@
+public class Operadores {
+    public static void main (String[]args){
+       boolean condicao1 = false
+       ;   
+       boolean condicao2 = true;
+       
+       if (condicao1 && (7>4)){
+            System.out.println("As duas condições são veradeiras");
+       }
+
+       if (condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira");
+       }
+       else
+            System.out.println("Nenhuma das condições é veradeira");
+       System.out.println("fim");
+
+        
+    } 
+
+}
